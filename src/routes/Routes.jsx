6267d778c52232bfefa44218754936252fea9eb6
mdnumanahmed <mnumansyl@gmail.com>
2023-06-10 02:21:37 +0600

    created AuthProvider and used in main.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home/Home";
 import { createBrowserRouter } from "react-router-dom";
+import SignUp from "../pages/Login/SignUp";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: 'signup',
+        element: <SignUp />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      }
     ],
   },
 ]);
